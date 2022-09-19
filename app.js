46d7e268ddcbaa53917cad410d5c5fbc72f8474e
Nurullah Arslan <codarscoder@gmail.com>
2022-09-19 20:11:03 +0300

    card-wrapper pages

---
+++ app.js
@@ -0,0 +1,37 @@
+
+        // var opacity = 0;
+        // var intervalID = 0;
+        // window.onload = fadeIn();
+        //     let sayim = 0;
+        //     let say = 0;
+        // function fadeIn() {
+        //     setInterval(show, 200);
+        // }
+        // function fadeN() {
+        //    say =  setInterval(left, 100);
+        // }  
+  
+        // function show() {
+        //     var body = document.getElementById("body");
+            
+        //     if (opacity <= 1) {
+        //         opacity = opacity + 0.1;
+        //         body.style.opacity = opacity
+        //     } else {
+        //         clearInterval(intervalID);
+        //     }
+        // }
+
+        // function left(){
+        //     let card = document.getElementById("card-wrap");
+           
+        //     if (sayim < 6) {
+        //         let dizi = ["10px","20px","30px","40px","50px","60px"]
+        //         sayim ++
+
+        //          card.style.left =dizi[sayim]   //`"${sayim}px"`;
+        //     } else {
+        //         clearInterval(say);
+        //     }
+        // }fadeN()
+       
